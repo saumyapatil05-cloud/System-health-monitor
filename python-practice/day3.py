82372342194413b7de7979e2518358826f7f1d30
saumya <saumyapatil05@gamil.com>
2026-07-25 23:49:39 +0530

    if else conditions

---
+++ python-practice/day3.py
@@ -0,0 +1,29 @@
+#print("Input your age:")
+#age = int (input())
+#
+#if age>=16:
+#	print("hey you're 16")
+#else:
+#	print("You are younger than 16")
+#height=int(input())
+#if (height)<1:
+#	print("you cannot ride")
+#elif (height)>2:
+#	print("you cannot ride,over 2m")
+#else:
+#	print('you can ride')
+#x=3
+#y=2
+#if not(y==x or x+y==5):
+#	print('ran')
+#else:
+#	print(':(')
+x=2
+y=3
+if x==2:
+	if y==3:
+		print("all correct")
+	else:
+		print("one correct")
+else:
+	print("both incorrect")
