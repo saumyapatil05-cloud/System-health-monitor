4096b1d20d44188eae4ae4c78adc6a2de2b2d755
saumya <saumyapatil05@gamil.com>
2026-07-26 09:41:25 +0530

    Strings

---
+++ python-practice/day6.py
@@ -0,0 +1,10 @@
+# .strip(), len(), .lower(), .upper(), .split()
+
+text =input("input something:")
+#print(text.strip())  #removes  white spaces before and ater word
+
+print(len(text))
+print(text.lower())
+print(text.upper()) #everything in upper and lowercase
+
+print(text.split(" "))   #the space is ignored and made into a list
