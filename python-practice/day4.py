cc3e419b0bc93e734d2ab891c28dc6608926e830
saumya <saumyapatil05@gamil.com>
2026-07-26 09:12:46 +0530

    for loop,while,lists,tuples

---
+++ python-practice/day4.py
@@ -0,0 +1,22 @@
+#for x in range(10,-1,-1): #(start,stop.step or incr or decr)
+#	print(x)
+
+#while condition==True:
+#	do this
+
+#loop=True
+#while loop:
+#	name=input("Insert something: ")
+#	if name=="stop":
+#	   loop=False
+#	   break
+
+fruits=["apple","pear","orange"]
+print(fruits)   #to access the items in list we will use fruits[0],apple will be accessed
+
+fruits.append("litchi") #append takes only one argument
+print(fruits)
+
+fruits[1]='berry'
+print(fruits)
+
